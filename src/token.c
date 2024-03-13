cd4449b71635e06bdcf93428c0fc21150d1ecf56
Luke Lai <llai@c3r2s9.42london.com>
2024-03-13 16:40:45 +0000

    Add > and < sym for tokenize

--- src/token.c
+++ src/token.c
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 18:08:50 by llai              #+#    #+#             */
-/*   Updated: 2024/03/13 16:30:22 by llai             ###   ########.fr       */
+/*   Updated: 2024/03/13 16:38:28 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,25 @@ void	tokenize(char *cmd_line, t_list **tk_list)
 	// str = ft_strdup("");
 	while (*cmd_line != '\0')
 	{
-		if (ft_isalpha(*cmd_line) || *cmd_line == '-')
+		if (ft_isalnum(*cmd_line) || *cmd_line == '-' || *cmd_line == '_')
 		{
 			ft_lstadd_back(&tmp, ft_lstnew(ft_strdup(&(*cmd_line))));
+		}
+		else if (*cmd_line == '<')
+		{
+			ft_lstadd_back(&tmp, ft_lstnew(ft_strdup(&(*cmd_line))));
+			tmp_to_tokens(tk_list, tmp);
+			ft_lstclear(&tmp, free);
+			cmd_line++;
+			continue;
+		}
+		else if (*cmd_line == '>')
+		{
+			ft_lstadd_back(&tmp, ft_lstnew(ft_strdup(&(*cmd_line))));
+			tmp_to_tokens(tk_list, tmp);
+			ft_lstclear(&tmp, free);
+			cmd_line++;
+			continue;
 		}
 		cmd_line++;
 		if (*cmd_line == ' ' || *cmd_line == '\0')
